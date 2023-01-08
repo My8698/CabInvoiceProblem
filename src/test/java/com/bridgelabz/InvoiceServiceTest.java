7ceffb7c315fa4ba0a47624d3dd7e4f2902f784c
devraajmadhuri@gmail.com <devraajmadhuri@gmail.com>
2023-01-08 21:21:19 +0530

    Added:calculate total fare

---
+++ src/test/java/com/bridgelabz/InvoiceServiceTest.java
@@ -0,0 +1,30 @@
+package com.bridgelabz;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InvoiceServiceTest {
+
+    @Test
+    public void givenDistanceAndTime_ShouldReturnTotalFare() {
+        CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
+        double distance = 2.0;
+        int time = 5;
+
+        /**
+         * Calculating the fair
+         */
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(25.0, fare,0.0);
+    }
+    /*
+   Step 2 Test Case for minimum fare should give 5
+   */
+    @Test
+    public void givenDistanceAndTime_WhenTotalFareLessThan10_ShouldReturnMinimumFare() {
+        CabInvoiceGenerator invoiceGenerator = new CabInvoiceGenerator();
+        double distance = 0.1;
+        int time = 1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5.0, fare,0.0);
+    }
+}
